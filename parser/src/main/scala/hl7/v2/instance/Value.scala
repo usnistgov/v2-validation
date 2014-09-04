bb2dd4f17acceb9aeeda1febdac6f7ba0c45deb8
Salifou Sidi M. Malick <salifou.sidi@gmail.com>
2014-09-04 12:09:56 -0400

    Implemented expression domain object and XML deserialization

--- parser/src/main/scala/hl7/v2/instance/Value.scala
+++ parser/src/main/scala/hl7/v2/instance/Value.scala
@@ -1,5 +1,6 @@
 package hl7.v2.instance
 
+import scala.util.Try
 
 /**
   * @author Salifou Sidi M. Malick <salifou.sidi@gmail.com>
@@ -7,6 +8,15 @@ package hl7.v2.instance
 
 sealed trait Value {
   def asString: String
+
+  /**
+    * Compares this to v and returns :
+    *   Success( 1 ) if this is greater that `v'
+    *   Success(-1 ) if this is lower that `v'
+    *   Success( 0 ) if this is equal to `v'
+    *   Failure if `v' cannot be converted to the current Value class or if the comparison is not permitted.
+    */
+  def compareTo(v: Value): Try[Int] = ??? //FIXME
 }
 
 case class Number(asString: String)   extends Value
---
+++ validation/src/main/scala/expression/XMLDeserializer.scala
@@ -0,0 +1,132 @@
+package expression
+
+import nu.xom.Element
+import nist.xml.util.XOMExtensions._
+import hl7.v2.instance.Text
+import hl7.v2.instance.Number
+
+/**
+  * @author Salifou Sidi M. Malick <salifou.sidi@gmail.com>
+  */
+
+object XMLDeserializer {
+
+  /**
+    * Creates an expression from a nu.xom.Element representing an assertion
+    */
+  //def assertion( e: Element ): Expression = expression( e.getChildElements().get(0) )
+
+  /**
+    * Creates an expression from a nu.xom.Element representing the expression
+    */
+  def expression( e: Element ): Expression = e.getLocalName match {
+    case "Presence"    => presence( e )
+    case "PathValue"   => pathValue( e )
+    case "PlainText"   => plainText( e )
+    case "Format"      => format( e )
+    case "NumberList"  => numberList( e )
+    case "StringList"  => stringList( e )
+    case "SimpleValue" => simpleValue( e )
+    case "NOT"         => not( e )
+    case "AND"         => and( e )
+    case "OR"          => or( e )
+    case "XOR"         => xor( e )
+    case "IMPLY"       => imply( e )
+    case "FORALL"      => forall( e )
+    case "EXIST"       => exist( e )
+    case _ => throw new Error(s"[Error] Unkown expression node $e")
+  } 
+
+  // Generic Expressions
+  private def presence( e: Element ) = Presence( e.attribute("Path") )
+
+  private def pathValue( e: Element ) = {
+    val path1 = e.attribute("Path1")
+    val path2 = e.attribute("Path2")
+    val op    = operator( e.attribute("Operator") )
+    PathValue( path1, op, path2 )
+  }
+
+  // Value Expressions
+  private def plainText( e: Element ): PlainText = {
+    val path = e.attribute("Path")
+    val text = e.attribute("Text")
+    val ignoreCase = toBoolean( e.attribute("IgnoreCase") )
+    PlainText( path , text, ignoreCase )
+  }
+
+  private def format( e: Element ): Format = {
+    val path = e.attribute("Path")
+    val regex = e.attribute("Regex")
+    Format( path, regex)
+  }
+
+  private def numberList( e: Element ): NumberList = {
+    val path = e.attribute("Path")
+    val csv  = e.attribute("CSV").split(',').toList map ( _.toDouble )
+    NumberList( path, csv )
+  }
+
+  private def stringList( e: Element ): StringList = {
+    val path = e.attribute("Path")
+    val csv  = e.attribute("CSV").split(',').toList //No need to trim since no spaces in the schema
+    StringList( path, csv )
+  }
+
+  private def simpleValue( e: Element ): SimpleValue = {
+    val path = e.attribute("Path")
+    val op   = operator( e.attribute("Operator") )
+    val _value = value( e.attribute("Value"), e.attribute("Type") )
+    SimpleValue( path, op, _value )
+  }
+
+  // Combination Expressions
+  private def not( e: Element ) = NOT( expression( e.getChildElements.get(0) ) )
+
+  private def and( e: Element ) = AND.tupled( combination2(e) )
+
+  private def or( e: Element )  = OR.tupled( combination2(e) )
+
+  private def xor( e: Element ) = XOR.tupled( combination2(e) )
+
+  private def imply( e: Element )  = IMPLY.tupled( combination2(e) )
+
+  private def forall( e: Element ) = FORALL( combinationn(e): _ * )
+
+  private def exist( e: Element )  = EXIST( combinationn(e): _ * )
+
+  // Helpers
+  private def toBoolean( s: String ) = 
+    if( "true" == s || "1" == s ) true
+    else if( "false" == s || "0" == s ) false
+    else throw new Error(s"[Error] Invalid XSD:Boolean value $s")
+
+  private def operator( v: String ) = v match {
+    case "EQ" => Operator.EQ
+    case "NE" => Operator.NE
+    case "GT" => Operator.GT
+    case "LT" => Operator.LT
+    case "GE" => Operator.GE
+    case "LE" => Operator.LE
+    case   _  => throw new Error(s"[Error] Invalid Operator $v")
+  }
+
+  private def value( v: String, typ: String ) = 
+    if( "" == typ || "String" == typ ) Text(v)
+    else if( "Number" == typ ) Number(v)
+    else throw new Error(s"[Error] Unsupported value $v")
+
+  private def combination2( e: Element ) = {
+    val childElements = e.getChildElements
+    val exp1 = expression( childElements.get(0) )
+    val exp2 = expression( childElements.get(1) )
+    ( exp1, exp2)
+  }
+
+  /*private def combinationn( e: Element ) = {
+    val childElements = e.getChildElements
+    ( 0 until childElements.size ) map ( i => expression( childElements.get( i ) ) )
+  }*/
+
+  private def combinationn( e: Element ) = e.getChildElements map expression
+}
