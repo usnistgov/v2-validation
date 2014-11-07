65f9c8f4db20632bf3abe02d3b6964ebe8c48545
Salifou Sidi M. Malick <salifou.sidi@gmail.com>
2014-11-07 12:48:24 -0500

    Implemented segment creation

--- parser/src/main/scala/hl7/v2/instance/Segment.scala
+++ parser/src/main/scala/hl7/v2/instance/Segment.scala
@@ -1,6 +1,6 @@
 package hl7.v2.instance
 
-import hl7.v2.profile.{SegmentRef => SM}
+import hl7.v2.profile.{SegmentRef => SM, Field => FM}
 
 /**
   * Class representing a segment
@@ -13,7 +13,7 @@ case class Segment (
     hasExtra: Boolean
 ) extends SegOrGroup
 
-/*object Segment {
+object Segment {
 
   /**
     * Creates and returns a segment
@@ -22,80 +22,79 @@ case class Segment (
     * @param p - The position
     * @param i - The instance number
     * @param l - The line number
-    * @param map - The data type map
     * @return A segment
     */
-  def apply(m: SM, v: String, p: Int, i: Int, l: Int)
-           (implicit map: Map[String, Datatype]): Segment = {
-
+  def apply(m: SM, v: String, p: Int, i: Int, l: Int): Segment = {
     require( isValid( v ), s"Invalid segment instance '$v'" )
-    require( m.name == v.take(3),
-      s"Segment name mismatch. Expected: '${m.name}', Found: '$v'" )
-
-    val loc  = Location( m.desc, s"${m.name}[$i]", l, column=1 )
-    val fields: List[Field] = Nil
-    val hasExtra = false //FIXME
-
-    val fml = m.fields
+    val name = m.ref.name
+    require(name == v.take(3), s"Invalid segment name. Expected: '$name', Found: '$v'")
+    val loc = Location(m.ref.desc, s"$name[$i]", l, 1)
+    val fml = m.ref.fields
     val vs  = split( fs, v drop 4 , 5)
-
-    Segment(m.qProps, loc, p, i, fields, m.requirements, hasExtra)
+    val (hasExtra, lfs) =
+      if( v.startsWith("MSH") ) (vs.size > fml.size - 2, mshFields(fml, vs, loc))
+      else (vs.size > fml.size - 2, fields( fml, vs, loc ))
+    Segment(m, loc, i, lfs.flatten, hasExtra)
   }
 
-
-  private val segFormat = s"[A-Z]{2}[A-Z0-9](?:\\Q$fs\\E.*)*".r
-
-  private def isValid( s: String ) = segFormat.pattern.matcher( s ).matches
-
-}*/
-
-
-
-
-/*
- /**
-    * Creates and returns a segment instance from the string
-    * @param model - The segment model
-    * @param value - The string value
-    * @param instance - The instance number
-    * @param line - The line number
-    * @return The segment instance
-    */
-  def apply(model: SM, value: String, instance: Int, line: Int ): Segment = {
-    require( isValid( value ), s"Invalid segment instance '$value'" )
-    val name = model.ref.name
-    require( name == value.take(3),
-        s"Segment model and instance name mismatch. Expected: '$name', Found: '$value'" )
-    val l   = Location(s"$name[$instance]", line, 1)
-    val fml = model.ref.fields
-    val vs  = split( fs, value drop 4 , 5)
-    val _fields = if( value.startsWith("MSH") ) mshFields(fml, vs, l) else fields( fml, vs, l )
-    Segment( model, _fields, instance, l )
-  }
-
-  def apply(model: SM, line: (Int, String), instance: Int): Segment =
-    apply(model, line._2, instance, line._1)
+  /**
+    * Creates abd returns a list of field
+   * @param fml - The list of field models
+   * @param vs  - The value and column array
+   * @param l   - The parent location
+   * @return A list of field
+   */
+  private def fields( fml: List[FM], vs: Array[(Int, String)], l: Location) =
+    fml zip vs map { t => repetitions( t._1, t._2, l ) }
 
   private def mshFields( fml: List[FM], vs: Array[(Int, String)], l: Location ) = {
     val separators = vs(0)._2
-    require( separators matches( """\Q^\E~\Q\\E&(?:#.*)?""" ) ,
-        s"Invalid HL7 separators. Expected '^~\\&' or '^~\\&#'. Found: '$separators'" )
+    require( separators matches( """\Q^\E~\Q\\E&(?:#.*)?""" ) , s"Invalid HL7 separators. Expected '^~\\&' or '^~\\&#'. Found: '$separators'" )
     val _fields = fields(fml.tail.tail, vs drop 1 , l)
-    val `MSH.1` = DataElementBuilder(fml.head, s"$fs", 1, location(l, 1, 4))
-    val `MSH.2` = DataElementBuilder(fml.tail.head, s"$separators", 1, location(l, 2, 5))
-    ( `MSH.1`.toList ) :: ( `MSH.2`.toList ) :: _fields
+    val `MSH.1` = DataElement(fml.head, s"$fs", location(l, "Field Separator", 1, 1, 4), 1) //FIXME use description from model
+    val `MSH.2` = DataElement(fml.tail.head, s"$separators", location(l, "Separators", 2, 1, 5), 1)
+    `MSH.1`.toList  :: `MSH.2`.toList :: _fields
   }
 
-  private def fields( fml: List[FM], vs: Array[(Int, String)], l: Location) =
-    (fml zip pad(vs, (-1, ""), fml.size) ) map { t => repetitions( t._1, t._2, l ) }
 
-  private def repetitions( m:FM, t: (Int, String), l: Location): List[Field] =
-    ( split(rs, t._2, t._1).toList.zipWithIndex map { tt =>
-      val( (col, v), i ) = tt
-      DataElementBuilder( m, v, i + 1, location(l, m.position, col, i + 1) )
-    }).flatten
+  /**
+    * Creates and returns a list representing a repetition of a field
+    * @param m - The field model
+    * @param t - The column and value
+    * @param l - The parent location
+    * @return A list representing a repetition of a field
+    */
+  private def repetitions( m: FM, t: (Int, String), l: Location): List[Field] = {
+    val vs = split(rs, t._2, t._1)
+    val r = vs.toList.zipWithIndex map { tt =>
+      val( (col, v), ins) = tt
+      val loc = location(l, m.name, m.req.position, ins, col)
+      DataElement(m, v, loc, ins)
+    }
+    r.flatten
+  }
 
+  /**
+    * Regular expression for matching valid segment instance
+    */
   private val segFormat = s"[A-Z]{2}[A-Z0-9](?:\\Q$fs\\E.*)*".r
 
+  /**
+    * Returns true if s is a valid segment instance
+    * @param s - The segment as string
+    * @return True if s is a valid segment instance
+    */
   private def isValid( s: String ) = segFormat.pattern.matcher( s ).matches
-*/
+
+  /**
+    * Creates and returns a location from the parent location
+    * @param l - The parent location
+    * @param d - The current location description
+    * @param p - The current location position
+    * @param i - The current location instance
+    * @param c - The current location column
+    * @return A location
+    */
+  private def location(l: Location, d: String,  p: Int, i: Int, c: Int) =
+    l.copy( desc=d, path=s"${l.path}.$p[$i]", column = c )
+}
