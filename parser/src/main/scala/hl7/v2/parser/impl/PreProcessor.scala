0f1ab0e15b16ad2793a3425e296d80d530b1df69
Salifou Sidi M. Malick <salifou.sidi@gmail.com>
2014-11-08 13:48:11 -0500

    Cleaned the pre-processor

--- parser/src/main/scala/hl7/v2/parser/impl/PreProcessor.scala
+++ parser/src/main/scala/hl7/v2/parser/impl/PreProcessor.scala
@@ -1,4 +1,4 @@
-/*package hl7.v2.parser.impl
+package hl7.v2.parser.impl
 
 import scala.util.Try
 import scala.util.Failure
@@ -14,28 +14,41 @@ object PreProcessor {
     * Splits the message into lines and returns a pair of valid and invalid lines.
     * This function will standardize the lines i.e. it will replace the separators
     * with the ones recommended by HL7 if necessary.
-    * 
+    *
     * @param message - The message to be pre-processed
-    * @return A `Success' containing the pair of valid and invalid lines or a `Failure' if:
+    * @return A `Success' containing  valid and invalid lines or a `Failure' if:
     *    1) No MSH segment if defined in the message
     *    2) MSH segment contains less than 9 characters
     *    3) A character is use twice as a separator
     */
-  def process(message: String): Try[ Pair[List[Line], List[Line]] ] = splitOnMSH(message) match {
-    case (beforeMSH, Nil) => Failure( new Error("No MSH Segment found in the message.") )
-    case (beforeMSH, xs ) => getSeparators( xs.head._2 ) map { separators =>
-      implicit val fs = separators.fs
-      val (valid, invalid) = partition( xs )
-      ( standardize(valid, separators) , beforeMSH:::invalid ) //TODO Implement standardize
+  def process(message: String): Try[ Pair[List[Line], List[Line]] ] =
+    splitOnMSH(message) match {
+      case (beforeMSH, Nil) =>
+        Failure( new Error("No MSH Segment found in the message.") )
+      case (beforeMSH, xs ) =>
+        getSeparators( xs.head._2 ) map { separators =>
+          implicit val fs = separators.fs
+          val (valid, invalid) = partition( xs )
+          //TODO Implement standardize
+          ( standardize(valid, separators) , beforeMSH:::invalid )
+        }
     }
-  }
 
   /**
     * Splits the message into lines and returns a pair of list of lines.
     * The first list will contain all lines before the MSH segment
     */
-  private def splitOnMSH( message: String ) = 
-    ((Stream from 1) zip lineBreak.split( message )).toList span ( l => !(l._2 startsWith "MSH") )
+  private def splitOnMSH( message: String ): (List[Line], List[Line]) =
+    ( (Stream from 1) zip lineBreak.split( message ) ).toList span { l =>
+      !(l._2 startsWith "MSH")
+    }
+
+  /**
+    * Partition the list of lines into list of valid and invalid lines.
+    */
+  private def partition(list: List[Line])
+                       (implicit fs: Char): (List[Line], List[Line]) =
+    list partition( l => validLinesRegex.pattern.matcher( l._2 ).matches )
 
   /**
     * Returns the separators defined in MSH.2 or a Failure
@@ -48,28 +61,23 @@ object PreProcessor {
     val ss = msh(7)
     val x  = msh(8)
     val tc = if( x == fs ) None else Some(x)
-    val seps = Separators(fs, cs, rs, ec, ss, tc)
-    if( seps.getDuplicates.nonEmpty ) Failure( new Error( s"The following character(s) ['${
-        seps.getDuplicates.mkString("','") }'] has/have been used more than once as a separator." ) )
-    else Success( seps )
-  } catch { 
+    val separators = Separators(fs, cs, rs, ec, ss, tc)
+    separators.getDuplicates match {
+      case Nil => Success( separators )
+      case xs  => Failure(
+        new Error( s"The following character(s) ['${xs.mkString("', '")
+        }'] has/have been used more than once as a separator.")
+      )
+    }
+  } catch {
     case _: Throwable => Failure( new Error("The MSH line contains less than 9 characters.") )
-  }
-
-  /**
-    * Partition the list of lines into list of valid and invalid lines.
-    */
-  private def partition(list: List[(Int,String)])(implicit fs: Char) = {
-    //FIXME Should we allow blank after the field separator?
-    val validLinesRegex = s"^[A-Z]{2}[A-Z0-9](${quote(fs)}.+)*".r 
-    list partition( l => validLinesRegex.pattern.matcher( l._2 ).matches )
   }
 
   /**
     * Standardizes the lines of lines i.e. replace the separators with
     * the ones recommended by HL7 is necessary
     */
-  private def standardize(list: List[(Int,String)], seps: Separators): List[(Int,String)] = 
+  private def standardize(list: List[Line], seps: Separators): List[Line] =
     if( seps.areRecommended ) list else ??? //TODO list map ( standardize( _, seps) )
 
   /*
@@ -79,7 +87,7 @@ object PreProcessor {
     def splitAndReplace(s: String, l: List[(Char, Char)]): String = l match {
       case Nil         => s
       case (x, y)::Nil => s.replaceAll(quote(x), y.toString)
-      case (x, y)::xs  => s.split(x) map( splitAndReplace(_, xs) ) mkString (y.toString) 
+      case (x, y)::xs  => s.split(x) map( splitAndReplace(_, xs) ) mkString (y.toString)
     }
     val l = seps.toList zip List('|', '^', '~', '\\', '&', '#')
     if( line._2 startsWith "MSH" ) seps.tc match {
@@ -88,4 +96,5 @@ object PreProcessor {
     } else ( line._1, splitAndReplace( line._2 , l) )
   }*/
 
-}*/
+}
+
