aa484bbae61bec2cf74d067b72c8df64074da202
Salifou Sidi M. Malick <salifou.sidi@gmail.com>
2014-11-27 21:07:57 -0500

    Removed extra spaces from ValueComparisonGuardSpec

--- parser/src/test/scala/hl7/v2/instance/extension/ValueComparisonGuardsSpec.scala
+++ parser/src/test/scala/hl7/v2/instance/extension/ValueComparisonGuardsSpec.scala
@@ -55,52 +55,52 @@ class ValueComparisonGuardsSpec extends Specification { def is = s2"""
 
   val dtz = Some(TimeZone("+0000"))
 
-  val text     = Text("x")
-  val ftext    = FText("x")
-  val number   = Number("1")
-  val date     = Date("2014")
-  val time     = Time("00", dtz)
+  val text   = Text("x")
+  val ftext  = FText("x")
+  val number = Number("1")
+  val date   = Date("2014")
+  val time   = Time("00", dtz)
   val dateTime = DateTime("2014", dtz)
 
-  def text_text     = text isComparableWith text     must beTrue
-  def text_ftext    = text isComparableWith ftext    must beTrue
-  def text_number   = text isComparableWith number   must beTrue
-  def text_date     = text isComparableWith date     must beTrue
-  def text_time     = text isComparableWith time     must beTrue
+  def text_text   = text isComparableWith text   must beTrue
+  def text_ftext  = text isComparableWith ftext  must beTrue
+  def text_number = text isComparableWith number must beTrue
+  def text_date   = text isComparableWith date   must beTrue
+  def text_time   = text isComparableWith time   must beTrue
   def text_dateTime = text isComparableWith dateTime must beTrue
 
-  def ftext_text     = ftext isComparableWith text     must beTrue
-  def ftext_ftext    = ftext isComparableWith ftext    must beTrue
-  def ftext_number   = ftext isComparableWith number   must beTrue
-  def ftext_date     = ftext isComparableWith date     must beTrue
-  def ftext_time     = ftext isComparableWith time     must beTrue
+  def ftext_text   = ftext isComparableWith text   must beTrue
+  def ftext_ftext  = ftext isComparableWith ftext  must beTrue
+  def ftext_number = ftext isComparableWith number must beTrue
+  def ftext_date   = ftext isComparableWith date   must beTrue
+  def ftext_time   = ftext isComparableWith time   must beTrue
   def ftext_dateTime = ftext isComparableWith dateTime must beTrue
 
-  def number_text     = number isComparableWith text     must beTrue
-  def number_ftext    = number isComparableWith ftext    must beTrue
-  def number_number   = number isComparableWith number   must beTrue
-  def number_date     = number isComparableWith date     must beFalse
-  def number_time     = number isComparableWith time     must beFalse
+  def number_text   = number isComparableWith text   must beTrue
+  def number_ftext  = number isComparableWith ftext  must beTrue
+  def number_number = number isComparableWith number must beTrue
+  def number_date   = number isComparableWith date   must beFalse
+  def number_time   = number isComparableWith time   must beFalse
   def number_dateTime = number isComparableWith dateTime must beFalse
 
-  def date_text     = date isComparableWith text     must beTrue
-  def date_ftext    = date isComparableWith ftext    must beTrue
-  def date_number   = date isComparableWith number   must beFalse
-  def date_date     = date isComparableWith date     must beTrue
-  def date_time     = date isComparableWith time     must beFalse
+  def date_text   = date isComparableWith text   must beTrue
+  def date_ftext  = date isComparableWith ftext  must beTrue
+  def date_number = date isComparableWith number must beFalse
+  def date_date   = date isComparableWith date   must beTrue
+  def date_time   = date isComparableWith time   must beFalse
   def date_dateTime = date isComparableWith dateTime must beFalse
 
-  def time_text     = time isComparableWith text     must beTrue
-  def time_ftext    = time isComparableWith ftext    must beTrue
-  def time_number   = time isComparableWith number   must beFalse
-  def time_date     = time isComparableWith date     must beFalse
-  def time_time     = time isComparableWith time     must beTrue
+  def time_text   = time isComparableWith text   must beTrue
+  def time_ftext  = time isComparableWith ftext  must beTrue
+  def time_number = time isComparableWith number must beFalse
+  def time_date   = time isComparableWith date   must beFalse
+  def time_time   = time isComparableWith time   must beTrue
   def time_dateTime = time isComparableWith dateTime must beFalse
 
-  def dateTime_text     = dateTime isComparableWith text     must beTrue
-  def dateTime_ftext    = dateTime isComparableWith ftext    must beTrue
-  def dateTime_number   = dateTime isComparableWith number   must beFalse
-  def dateTime_date     = dateTime isComparableWith date     must beFalse
-  def dateTime_time     = dateTime isComparableWith time     must beFalse
+  def dateTime_text   = dateTime isComparableWith text   must beTrue
+  def dateTime_ftext  = dateTime isComparableWith ftext  must beTrue
+  def dateTime_number = dateTime isComparableWith number must beFalse
+  def dateTime_date   = dateTime isComparableWith date   must beFalse
+  def dateTime_time   = dateTime isComparableWith time   must beFalse
   def dateTime_dateTime = dateTime isComparableWith dateTime must beTrue
 }
