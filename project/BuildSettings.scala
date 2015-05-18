3aefff6d48cd34193969b5c608fbbf863a89aed7
Salifou Sidi M. Malick <salifou.sidi@gmail.com>
2015-05-18 11:47:34 -0400

    Updated the project version and reference.conf

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildSettings {
 
   lazy val basicSettings = Seq(
-    version              := "1.0.0-SNAPSHOT",
+    version              := "1.0.0-beta-0",
     //FIXME homepage             := Some(new URL("xxx")),
     organization         := "gov.nist",
     organizationHomepage := Some(new URL("http://nist.gov")),
