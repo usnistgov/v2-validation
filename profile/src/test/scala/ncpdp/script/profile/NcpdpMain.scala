8706342260f92406db6803afceded17d2b726b98
Frederic de Vaulx <fdevaulx@nist.gov>
2015-08-18 10:14:50 -0400

    added profile drafts

---
+++ profile/src/test/scala/ncpdp/script/profile/NcpdpMain.scala
@@ -0,0 +1,22 @@
+package ncpdp.script.profile
+
+import scala.util.{Failure, Success}
+import scala.xml.PrettyPrinter
+
+import hl7.v2.profile.XMLSerializer
+
+object Main extends App {
+
+  println("NCPDP Main: " + (args mkString ", "))
+
+  val profilePath = if (args.length == 0) null else args(0)
+	println("profilePath: " + profilePath)
+
+  val xml = if (profilePath != null) getClass.getResourceAsStream("/"+profilePath) else getClass.getResourceAsStream("/Profile-Ncpdp-Newrx.xml")
+  XMLDeserializer.deserialize(xml) match {
+    case Success(s) => 
+      val pp = new PrettyPrinter(200, 4)
+      println( pp.format( XMLSerializer.serialize( s ) ) )
+    case Failure(f) => println("Error Message:\n"+f.getMessage)
+  }
+}
