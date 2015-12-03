eefab308a448406d05de416bb05fa88b68dff4e6
Lefort, Maxence C <maxence.lefort@nist.gov>
2015-12-03 00:40:01 -0500

    Update version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildSettings {
 
   lazy val basicSettings = Seq(
-    version              := "1.0.0-beta-12.1-SNAPSHOT",
+    version              := "1.0.0-beta-12.1-N-SNAPSHOT",
     //FIXME homepage             := Some(new URL("xxx")),
     organization         := "gov.nist",
     organizationHomepage := Some(new URL("http://nist.gov")),
