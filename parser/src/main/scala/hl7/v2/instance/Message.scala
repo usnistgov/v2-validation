0addd92b0247df2a13f232bdbf8c08757140dc32
Lefort, Maxence C <maxence.lefort@nist.gov>
2016-01-14 17:48:35 -0500

    Add toString method to rebuild a message from the model

--- parser/src/main/scala/hl7/v2/instance/Message.scala
+++ parser/src/main/scala/hl7/v2/instance/Message.scala
@@ -1,6 +1,9 @@
 package hl7.v2.instance
 
-import hl7.v2.profile.{Message => MM}
+import hl7.v2.profile.{Message => MM, Req, Composite}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Failure
 
 /**
   * Class representing a message
@@ -17,5 +20,126 @@ case class Message(
   lazy val asGroup = Group( model.asGroup, 1, children)
 
   lazy val location = asGroup.location
+
+  override def toString(): String = {
+    var message = ""
+    var isFirst = true
+    for(segOrGroup <- children) {
+      message+=segOrGroupAsString(segOrGroup,isFirst)
+      isFirst=false
+    }
+    message
+  }
+
+  def findFieldsInSegment(req: Req, fieldList: List[Field]) : ArrayBuffer[Field] = {
+    var fields = ArrayBuffer[Field]()
+    for(field <- fieldList){
+      if(req==field.req){
+        fields+=field
+      }
+    }
+    fields
+  }
+
+  def segOrGroupAsString(segOrGroup: SegOrGroup, isFirstSegOrGroup: Boolean) : String = {
+    if(segOrGroup.isInstanceOf[Group]){
+      var res = ""
+      for(subSegOrGroup <- (segOrGroup.asInstanceOf[Group]).children){
+        res += segOrGroupAsString(subSegOrGroup,false)
+      }
+      res
+    } else {
+      val segment = segOrGroup.asInstanceOf[Segment]
+      var res = segment.model.ref.name
+      if(!isFirstSegOrGroup){
+        res+=separators.fs
+      }
+      var isFirst = true
+      for (field <- segment.model.ref.fields) {
+        val fieldsInSegment = findFieldsInSegment(field.req,segment.children)
+        if(fieldsInSegment.length>0){
+          if(!isFirst) {
+            res += separators.fs
+          }
+          var isFirstFieldInSegment = true
+          for(fieldInSegment <- fieldsInSegment){
+            if(!isFirstFieldInSegment){
+              res += separators.rs
+            }
+            res += fieldAsString(fieldInSegment)
+            isFirstFieldInSegment=false
+          }
+          isFirst=false
+        } else {
+          res += separators.fs
+        }
+      }
+      while(res.endsWith(""+separators.fs)){
+        res = res.substring(0,res.length-1)
+      }
+      if(!isFirstSegOrGroup){
+        res+=separators.ts.get
+      }
+      res+"\n"
+    }
+  }
+
+  def findSimpleComponentInField(req:Req,components:List[Component]):Option[Component]={
+    for(component <- components){
+      if(req==component.req){
+        return Some(component)
+      }
+    }
+    None
+  }
+
+  def fieldAsString(field: Field): String ={
+    if(field.isInstanceOf[ComplexField]){
+      val complexField = field.asInstanceOf[ComplexField]
+      var res = ""
+      var isFirst = true
+      for(component <- complexField.datatype.components){
+
+        var componentInField = findSimpleComponentInField(component.req,complexField.children)
+        if(!componentInField.isEmpty){
+          if(!isFirst){
+            res+=separators.cs
+          }
+          res += componentAsString(componentInField.get)
+          isFirst = false
+        } else {
+          //if(component.req.usage!="X") {
+            res += separators.cs
+          //}
+        }
+      }
+      while(res.endsWith(""+separators.cs)){
+        res = res.substring(0,res.length-1)
+      }
+      res
+    } else {
+      val simpleField = field.asInstanceOf[SimpleField]
+      simpleField.value.raw
+    }
+  }
+
+  def componentAsString(component: Component):String = {
+    if(component.isInstanceOf[ComplexComponent]){
+      var res = ""
+      val complexComponent = component.asInstanceOf[ComplexComponent]
+      var isFirst=true
+      for(simpleComponent <- complexComponent.children){
+        if(!isFirst){
+          res += separators.ss
+        }
+        res += componentAsString(simpleComponent)
+        isFirst = false
+      }
+      res
+    } else {
+      val simpleComponent = component.asInstanceOf[SimpleComponent]
+      simpleComponent.value.raw
+    }
+  }
 
 }
