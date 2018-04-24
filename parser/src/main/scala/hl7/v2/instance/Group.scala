e26a171af5fd2c1cbab88064f673656fc0e8126e
Tamri, Hossam (IntlAssoc) <hossam.tamri@nist.gov>
2018-04-24 15:59:19 -0400

    Look forward Parsing when profile is not ambiguous

--- parser/src/main/scala/hl7/v2/instance/Group.scala
+++ parser/src/main/scala/hl7/v2/instance/Group.scala
@@ -1,29 +1,27 @@
 package hl7.v2.instance
 
-import hl7.v2.profile.{Group => GM}
+import hl7.v2.profile.{ Group => GM }
 
 /**
-  * Class representing a group
-  */
-case class Group (
+ * Class representing a group
+ */
+case class Group(
     model: GM,
     instance: Int,
-    children: List[SegOrGroup]
-) extends SegOrGroup {
+    children: List[SegOrGroup]) extends SegOrGroup {
 
   // The group should contain an element with position = 1 and instance = 1
-  require( children exists {c => c.position == 1 && c.instance == 1} )
+  // require(children exists { c => c.position == 1 && c.instance == 1 })
 
   val hasExtra = false //FIXME: A group cannot have extra unless it is the root right ?
 
   lazy val head: Segment =
-    children find { c => c.position == 1 && c.instance == 1 } match {
+    children headOption match {
       case Some(s: Segment) => s
-      case Some(g: Group)   => g.head
+      case Some(g: Group) => g.head
       case None => throw new Error(s"The group head is missing. $this")
       case Some(x) => throw new Error(s"Unknown Group element '$x'. $this")
     }
-
   lazy val location = head.location
-    .copy(EType.Group, desc=model.name, path = model.name, uidPath = s"${model.name}[$instance]")
+    .copy(EType.Group, desc = model.name, path = model.name, uidPath = s"${model.name}[$instance]")
 }
